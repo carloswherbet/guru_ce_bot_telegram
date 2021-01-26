50b1230be8ca6b57fbe4e404c407e59d36ba1609
Carlos <carloswherbet@gmail.com>
2021-01-26 11:04:42 -0300

    First commit

---
+++ bin/main.rb
@@ -0,0 +1,3 @@
+require_relative '../lib/bot.rb'
+require_relative '../lib/company.rb'
+Bot.new
---
+++ lib/db.rb
@@ -0,0 +1,17 @@
+require "sqlite3"
+
+# Open a database
+db = SQLite3::Database.new "guru_ce.db"
+
+# Create a table
+rows = db.execute <<-SQL
+  create table IF NOT EXISTS companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name varchar(30),
+    url varchar(255),
+    gov boolean,
+    createad_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    username varchar(255)
+  );
+SQL
+
